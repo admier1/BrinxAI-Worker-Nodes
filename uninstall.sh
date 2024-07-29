6ae74a93897ba40ec7df0a00b795681c57f955b5
admier1 <106995536+admier1@users.noreply.github.com>
2024-07-29 12:57:01 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,12 +3,25 @@
 # Set the user variable
 USR=${USR:-"brinxai_nodes"}
 
+# Define an array of container names
+containers=(
+  "${USR}-worker-1"
+  "root-worker-1"
+  "ai_worker_1"
+  "${USR}-rembg-1"
+  "root-rembg-1"
+  "ai_rembg_1"
+  "${USR}-text-ui-1"
+  "root-text-ui-1"
+  "ai_text-ui_1"
+)
+
 # Stop the existing containers
-docker stop ${USR}-worker-1 || docker stop root-worker-1
-docker stop ${USR}-rembg-1 || docker stop root-rembg-1
-docker stop ${USR}-text-ui-1 || docker stop root-text-ui-1
+for container in "${containers[@]}"; do
+  docker stop $container 2>/dev/null && echo "Stopped $container"
+done
 
 # Remove the existing containers
-docker rm ${USR}-worker-1 || docker rm root-worker-1
-docker rm ${USR}-rembg-1 || docker rm root-rembg-1
-docker rm ${USR}-text-ui-1 || docker rm root-text-ui-1
+for container in "${containers[@]}"; do
+  docker rm $container 2>/dev/null && echo "Removed $container"
+done
